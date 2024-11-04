0948125a2e1ff60d3bbfd3b48f0c2d0156f8e5a1
Sofia <sofia.salazar.m@utec.edu.pe>
2024-11-04 11:54:07 -0500

    weight tf-idf

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
+import pandas as pd
 import nltk
-import regex as re
+import math
+import json
+import re
 from nltk.stem.snowball import SnowballStemmer
+from collections import defaultdict
+import os
+
 nltk.download('punkt')
 stemmer = SnowballStemmer('spanish')
 
 with open("utils/stoplist.txt", encoding="latin1") as file:
     stoplist = [line.rstrip().lower() for line in file]
-stoplist += ['?', '-', '.', ':', ',', '!', ';'] 
+stoplist += ['?', '-', '.', ':', ',', '!', ';']
 
 def preProcesamiento(texto, stemming=False):
-    words = []
-
     texto = texto.lower()
-    texto = re.sub(r'[^a-zA-Z0-9_À-ÿ]', ' ', texto)
-    
+    texto = re.sub(r'[^a-zA-Z0-9_À-ÿ]', ' ', texto) 
     words = nltk.word_tokenize(texto)
     words = [word for word in words if word not in stoplist]
     if stemming:
         words = [stemmer.stem(word) for word in words]
+    
     return words
+
+def get_tf(word, doc):
+    return doc.count(word) / len(doc) if doc else 0
+
+def get_df(docs):
+    df = {}
+    for words in docs.values():
+        unique_words = set(words)  
+        for word in unique_words:
+            df[word] = df.get(word, 0) + 1
+    return df
+
+# mejor esquema de ponderación
+def w_tf_idf(tf, df, N):
+    return math.log10(1 + tf) * math.log10(N / df)
